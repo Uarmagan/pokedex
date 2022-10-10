52b93771aa923a337038d6d60a93cc90a15a7425
Ugur Armagan <ugurarmagan93@gmail.com>
2022-10-10 16:34:15 -0700

    basic Search Bar done

--- pages/v2.tsx
+++ pages/v2.tsx
@@ -1,8 +1,10 @@
+import { SearchBar } from '../components/search';
+
 export default function PokedexV2() {
   return (
     <main className='grid grid-cols-[2fr_1fr] h-screen py-8 gap-x-7'>
       <div className='flex flex-col gap-y-10'>
-        <div className='bg-purple-500 h-24 '>Search</div>
+        <SearchBar />
         <div className='bg-orange-500 h-16 '>Sorting</div>
         <div className='bg-green-400 h-16 '>Filtering</div>
         <div className='bg-teal-400 flex-auto'>Filtering</div>
